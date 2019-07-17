23cd67b9af5ac44453cfbee9ef7d9ace7be52eb8
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-07-17 16:40:37 -0700

    can POST a film using method and has passing test

---
+++ test/films.test.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Studio = require('../lib/models/Studio');
+const Film = require('../lib/models/Film');
+const Actor = require('../lib/models/Actor');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let studio = null;
+  let actor = null;
+  beforeEach(async() => {
+    studio = JSON.parse(JSON.stringify(await Studio.create({ name: 'disney' })));
+    actor = JSON.parse(JSON.stringify(await Actor.create({ name: 'robin williams' })));
+
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can POST a film', () => {
+    return request(app)
+      .post('/api/v1/films')
+      .send({ title: 'aladdin', studio: studio.name, released: 1992, cast: { role: '', actor: actor.name } })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'aladdin',
+          studio: studio.name,
+          released: 1992,
+          cast: { 
+            role: '',
+            actor: actor.name
+          },
+          __v: 0
+        });
+      });
+  });
+});
